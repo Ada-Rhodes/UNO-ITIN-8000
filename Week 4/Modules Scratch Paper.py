2b4b365b4d68758eed34e86d5bd05847fc01ff2b
Ada-Rhodes Short <29873439+Ada-Rhodes@users.noreply.github.com>
2021-09-15 19:21:07 -0500

    Improved Dicebag
    I improved dicebag to allow for multiple rolls and I need to update the deck to not allow it to over draw

--- Week 4/Modules Scratch Paper.py
+++ Week 4/Modules Scratch Paper.py
@@ -3,6 +3,6 @@ from toybox import dicebag as roll
 from toybox import carddeck as draw
 
 # Roll a six sided die
-print(roll.d6())
+print(roll.d20())
 # Draw a card
 print(draw.draw_cards(1))
--- toybox/dicebag.py
+++ toybox/dicebag.py
@@ -6,36 +6,57 @@ from random import randint as dieroll
 
 
 # 4 Sided Die
-def d4():
-    return dieroll(1, 4)
+def d4(n=1):
+    total = 0
+    for i in range(n):
+        total = total + dieroll(1, 4)
+    return total
 
 
 # 6 Sided Die
-def d6():
-    return dieroll(1, 6)
+def d6(n=1):
+    total = 0
+    for i in range(n):
+        total = total + dieroll(1, 6)
+    return total
 
 
 # 8 Sided Die
-def d8():
-    return dieroll(1, 8)
+def d8(n=1):
+    total = 0
+    for i in range(n):
+        total = total + dieroll(1, 8)
+    return total
 
 
 # 10 Sided Die
-def d10():
-    return dieroll(1, 10)
+def d10(n=1):
+    total = 0
+    for i in range(n):
+        total = total + dieroll(1, 10)
+    return total
 
 
 # 12 Sided die
-def d12():
-    return dieroll(1, 12)
+def d12(n=1):
+    total = 0
+    for i in range(n):
+        total = total + dieroll(1, 12)
+    return total
 
 
 # 20 sided die
-def d20():
-    return dieroll(1, 20)
+def d20(n=1):
+    total = 0
+    for i in range(n):
+        total = total + dieroll(1, 20)
+    return total
 
 
 # 100 sided die or D-percentile
-def d100():
-    return dieroll(1, 100)
+def d100(n=1):
+    total = 0
+    for i in range(n):
+        total = total + dieroll(1, 100)
+    return total
 
