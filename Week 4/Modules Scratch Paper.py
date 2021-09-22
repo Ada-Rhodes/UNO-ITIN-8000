df3bbb26d0e7e9bedf4024ca4362948db61fa32a
Ada-Rhodes Short <29873439+Ada-Rhodes@users.noreply.github.com>
2021-09-22 17:54:39 -0500

    Example changes

--- Week 4/Modules Scratch Paper.py
+++ Week 4/Modules Scratch Paper.py
@@ -1,8 +1,16 @@
 # Import the dicebag
 from toybox import dicebag as roll
 from toybox import carddeck as draw
+import os
 
 # Roll a six sided die
 print(roll.d20())
 # Draw a card
 print(draw.draw_cards(1))
+
+#os.mkdir('poems')
+os.path.exists('poems')
+#os.mkdir('poems/mcintyre')
+
+print(os.listdir('poems'))
+os.rmdir('poems')
--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print(27)
+# Generate 256 byte values from 0 to 255
+poem = '''There was a young lady named Bright, 
+Whose speed was far faster than light; 
+She started one day 
+In a relative way, 
+And returned on the previous night.'''
+
+fout = open('relativity', 'wt')
+fout.write(poem)
+fout.close()
