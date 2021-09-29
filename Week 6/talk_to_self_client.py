b15c2d1ee194fc893b3fbba3d81ae5231c33bdbd
Ada-Rhodes Short <29873439+Ada-Rhodes@users.noreply.github.com>
2021-09-29 14:17:46 -0500

    more examples

---
+++ Week 6/talk_to_self_client.py
@@ -0,0 +1,14 @@
+import zmq
+
+host = '127.0.0.1'
+port = 6789
+context = zmq.Context()  # create a zmp Context object to maintain state
+client = context.socket(zmq.REQ)  # create a REQ (request) socket
+client.connect("tcp://%s:%s" % (host, port))  # connect to the server
+for num in range(1, 6):
+    request_str = input('Request Message:')
+    request_bytes = request_str.encode('utf-8')  # encode request string as utf-8
+    client.send(request_bytes)  # send string to server
+    reply_bytes = client.recv()  # receive reply
+    reply_str = reply_bytes.decode('utf-8')  # convert reply to utf-8
+    print("Sent: %s, received: %s" % (request_str, reply_str))  # print reply
---
+++ Week 6/talk_to_self_server.py
@@ -0,0 +1,19 @@
+import zmq  # you need to install with pip install pyzmq
+
+host = '127.0.0.1'
+port = 6789
+context = zmq.Context()  # the Context object is a ZMQ object that maintains state
+server = context.socket(zmq.REP)  # create a zmq socket of the REP (reply) type
+server.bind("tcp://%s:%s" % (host, port))  # bind server socket to host ip address and port
+while True:  # hold open and keep waiting for signals
+    #  Wait for next request from client
+    request_bytes = server.recv()  # get a request (message from client) and record it
+    request_str = request_bytes.decode('utf-8')  # decode it from utf-8 to a string
+    print(request_str)
+
+    reply_str = input('Reply Message:')  # prompt reply
+
+    reply_bytes = bytes(reply_str, 'utf-8')  # encode the reply to utf-8
+    server.send(reply_bytes)  # send the reply back to the client
+
+# what did we not do?
