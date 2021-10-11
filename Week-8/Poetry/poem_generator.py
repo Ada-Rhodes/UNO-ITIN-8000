56337ca8b670bfdd136fb28fe77b0fc7d899468c
Ada-Rhodes Short <29873439+Ada-Rhodes@users.noreply.github.com>
2021-10-11 16:54:54 -0500

    art demos

---
+++ Week-8/Poetry/poem_generator.py
@@ -0,0 +1,61 @@
+import os
+import pandas as pd
+import numpy as np
+import re
+import spacy
+
+
+# function to generate a poem
+def poem_generator(file, word, n_sents=4):
+    # load the english model from Spacy
+    nlp = spacy.load("en_core_web_trf")
+    init_str = nlp(word)
+    path = os.getcwd()
+    sentences = pd.read_csv(path + '\\' + file)
+    sup_index = sentences.shape[0]
+    poem_id = int()
+    poem = []
+    # generate the sentences
+    for i in range(n_sents):
+        rand_sent_index = np.random.randint(0, sup_index, size=30)
+        sent_list = list(sentences.sentence.iloc[rand_sent_index])
+        # transform sentences to a Spacy Doc object
+        docs = nlp.pipe(sent_list)
+        sim_list = []
+        # compute similarity for each sentence
+        for sent in docs:
+            similarity = (init_str.similarity(sent))
+            sim_list.append(similarity)
+        # saves similarity to DataFrame
+        df_1 = pd.DataFrame({'similarity': sim_list, 'doc_id': sentences.doc_id.iloc[rand_sent_index]},
+                            index=rand_sent_index)
+        df_1 = df_1[df_1.doc_id != poem_id]
+        df_1.sort_values(by='similarity', inplace=True, ascending=False)
+        sent_index = df_1.index[0]
+        sent = sentences.sentence[sent_index]
+        # erase line jumps and carriage return
+        replace_dict = {'\n': '', '\r': ''}
+        for x, y in replace_dict.items():
+            sent = sent.replace(x, y)
+        poem.append(sent)
+        poem_id = df_1.doc_id.iloc[0]
+        init_str = nlp(sent)
+        # join the sentences with a line break
+    str_poem = ("\n".join(poem))
+    return str_poem
+
+
+# generate a poem with initial word ='fear'
+poem = poem_generator(file='sentences_poems.csv', word='love')
+
+
+# function to uppercase first letter and add a dot to the end
+def format_poem(text):
+    text = text[:1].upper() + text[1:]
+    text = text[:-1] + '.'
+    return text
+
+
+# print the poem with new format
+final_poem = format_poem(poem)
+print(final_poem)
